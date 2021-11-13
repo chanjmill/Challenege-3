a0b50014b1b346d997be760fc967401a5d73dc71
Chandler <chanjmill@gmail.com>
2021-11-13 06:48:28 -0700

    prompt foundation set ie all of the prompts come when button is clicked, formatted and in order

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 // Assignment code here
+function prompts () {
+  var helloPrompt = window.alert("Hello! Complete the following prompts to generate your password.");
+
+  var lowerCasePrompt = window.prompt("CONFIRM if you want LOWER CASE letters in your password. Enter YES or NO.");
+  var upperCasePrompt = window.prompt("CONFIRM if you want UPPER CASE letters in your password. Enter YES or NO.");
+  var numericPrompt = window.prompt("CONFIRM if you want NUMBERS in your password. enter YES or NO.");
+  var specialCharPrompt = window.prompt("CONFIRM if you want SPECIAL CHARACTERS in your password. Enter YES or NO.");
+
+};
 
 
 // Get references to the #generate element
@@ -6,10 +15,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  prompts ();
 
 }
 
